8dd57d4075faf8091f60895e4cd361c4e770495f
maz2198 <marangmutloatse@gmail.com>
2020-05-31 16:40:51 +0200

    spark streaming commmit

---
+++ spark_streaming_twitter_5.py
@@ -0,0 +1,250 @@
+from pyspark import SparkConf,SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.sql import Row,SQLContext
+import sys
+import requests
+
+
+def aggregate_tags_count(new_values, total_sum):
+    return sum(new_values) + (total_sum or 0)
+
+def get_sql_context_instance(spark_context):
+    if ('sqlContextSingletonInstance' not in globals()):
+        globals()['sqlContextSingletonInstance'] = SQLContext(spark_context)
+    return globals()['sqlContextSingletonInstance']
+
+def process_rdd(time, rdd):
+    print("----------- %s -----------" % str(time))
+    try:
+# Get spark sql singleton context from the current context
+        sql_context = get_sql_context_instance(rdd.context)
+        # convert the RDD to Row RDD
+        row_rdd = rdd.map(lambda w: Row(word=w[0], word_count=w[1]))
+        # create a DF from the Row RDD
+        hashtags_df = sql_context.createDataFrame(row_rdd)
+        # Register the dataframe as table
+        hashtags_df.registerTempTable("words")
+        # get the top 10 hashtags from the table using SQL and print them
+        words_counts_df = sql_context.sql("select word, word_count from words order by word_count desc limit 10")
+        words_counts_df.show()
+    except:
+        e = sys.exc_info()[0]
+        print("Error: %s" % e)
+
+# create spark configuration
+conf = SparkConf()
+conf.setAppName("Maz_twitter_streaming_app")
+
+# create spark context with the above configuration
+sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")
+
+# create the Streaming Context from the above spark context with interval size 2 seconds
+ssc = StreamingContext(sc, 2)
+
+# setting a checkpoint to allow RDD recovery
+ssc.checkpoint("checkpointTwitterApp")
+
+# read data from port 9179
+dataStream = ssc.socketTextStream("localhost",9179)
+
+# Creating a list of stopwords - list from nltk stopwords
+stopwords = ['i',
+ 'me',
+ 'my',
+ 'myself',
+ 'we',
+ 'our',
+ 'ours',
+ 'ourselves',
+ 'you',
+ "you're",
+ "you've",
+ "you'll",
+ "you'd",
+ 'your',
+ 'yours',
+ 'yourself',
+ 'yourselves',
+ 'he',
+ 'him',
+ 'his',
+ 'himself',
+ 'she',
+ "she's",
+ 'her',
+ 'hers',
+ 'herself',
+ 'it',
+ "it's",
+ 'its',
+ 'itself',
+ 'they',
+ 'them',
+ 'their',
+ 'theirs',
+ 'themselves',
+ 'what',
+ 'which',
+ 'who',
+ 'whom',
+ 'this',
+ 'that',
+ "that'll",
+ 'these',
+ 'those',
+ 'am',
+ 'is',
+ 'are',
+ 'was',
+ 'were',
+ 'be',
+ 'been',
+ 'being',
+ 'have',
+ 'has',
+ 'had',
+ 'having',
+ 'do',
+ 'does',
+ 'did',
+ 'doing',
+ 'a',
+ 'an',
+ 'the',
+ 'and',
+ 'but',
+ 'if',
+ 'or',
+ 'because',
+ 'as',
+ 'until',
+ 'while',
+ 'of',
+ 'at',
+ 'by',
+ 'for',
+ 'with',
+ 'about',
+ 'against',
+ 'between',
+ 'into',
+ 'through',
+ 'during',
+ 'before',
+ 'after',
+ 'above',
+ 'below',
+ 'to',
+ 'from',
+ 'up',
+ 'down',
+ 'in',
+ 'out',
+ 'on',
+ 'off',
+ 'over',
+ 'under',
+ 'again',
+ 'further',
+ 'then',
+ 'once',
+ 'here',
+ 'there',
+ 'when',
+ 'where',
+ 'why',
+ 'how',
+ 'all',
+ 'any',
+ 'both',
+ 'each',
+ 'few',
+ 'more',
+ 'most',
+ 'other',
+ 'some',
+ 'such',
+ 'no',
+ 'nor',
+ 'not',
+ 'only',
+ 'own',
+ 'same',
+ 'so',
+ 'than',
+ 'too',
+ 'very',
+ 's',
+ 't',
+ 'can',
+ 'will',
+ 'just',
+ 'don',
+ "don't",
+ 'should',
+ "should've",
+ 'now',
+ 'd',
+ 'll',
+ 'm',
+ 'o',
+ 're',
+ 've',
+ 'y',
+ 'ain',
+ 'aren',
+ "aren't",
+ 'couldn',
+ "couldn't",
+ 'didn',
+ "didn't",
+ 'doesn',
+ "doesn't",
+ 'hadn',
+ "hadn't",
+ 'hasn',
+ "hasn't",
+ 'haven',
+ "haven't",
+ 'isn',
+ "isn't",
+ 'ma',
+ 'mightn',
+ "mightn't",
+ 'mustn',
+ "mustn't",
+ 'needn',
+ "needn't",
+ 'shan',
+ "shan't",
+ 'shouldn',
+ "shouldn't",
+ 'wasn',
+ "wasn't",
+ 'weren',
+ "weren't",
+ 'won',
+ "won't",
+ 'wouldn',
+ "wouldn't"]
+
+# split each tweet into words
+words = dataStream.flatMap(lambda line: line.split(" ")).map(lambda x: x.strip().lower()).filter(lambda word: word not in stopwords)
+
+# Count each word in each batch
+pairs = words.map(lambda word: (word, 1))
+
+# adding the count of each hashtag to its last count
+wordCounts = pairs.reduceByKeyAndWindow(lambda x, y: int(x) + int(y), lambda x, y: int(x) - int(y), 600, 30)
+wordCounts.pprint()
+
+# do processing for each RDD generated in each interval
+wordCounts.foreachRDD(process_rdd)
+
+
+# start the streaming computation
+ssc.start()
+
+# wait for the streaming to finish
+ssc.awaitTermination()
